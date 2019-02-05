6fc963f7265af813b0a8f0f707daec8e3c0b932b
Sarah Brown <sarah@transposit.com>
2019-02-05 23:28:38 +0000

    adds thes

--- word_search.js
+++ word_search.js
@@ -2,7 +2,11 @@
   var _ = require('underscore.js');
   const dict_results = api.run("this.def_and_syn", params);
   const results = api.run("this.extract_syn", {dict_results})
-  const possible_terms =  _.uniq(_.flatten(results.map(res => [res.id, ...res.syns])));
+  let possible_terms =  _.uniq(_.flatten(results.map(res => [res.id, ...res.syns])));
+  const thes_results = api.run("this.collegiate_thesaurus_search", params);
+  const flat_thes_results = _.flatten(thes_results.map(res => res.syns.slice(0, 3))); 
+  possible_terms = _.uniq(possible_terms.concat(flat_thes_results));
+  
   console.log("possible_terms", possible_terms);
   for (let i in possible_terms) {
     try {
