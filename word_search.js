04dabd56dc8696ff6acaa5f7e93874067c82494c
Sarah Brown <sarah@transposit.com>
2019-02-05 23:37:33 +0000

    Commit from Transposit IDE

--- word_search.js
+++ word_search.js
@@ -4,9 +4,8 @@
   const results = api.run("this.extract_syn", {dict_results})
   let possible_terms =  _.uniq(_.flatten(results.map(res => [res.id, ...res.syns])));
   const thes_results = api.run("this.collegiate_thesaurus_search", params);
-  const flat_thes_results = _.flatten(thes_results.map(res => res.syns.slice(0, 3))); 
+  const flat_thes_results = _.flatten(thes_results.map(res => res.syns.slice(0, 2))); 
   possible_terms = _.uniq(possible_terms.concat(flat_thes_results));
-  
   console.log("possible_terms", possible_terms);
   for (let i in possible_terms) {
     const sanitized = possible_terms[i].toLowerCase().replace(" ", "").replace("-", "");
