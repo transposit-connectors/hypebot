a07730591791fb9bd11cb4d817c19242d5e9dc30
Sarah Brown <sarah@transposit.com>
2019-02-05 22:32:32 +0000

    Commit from Transposit IDE

--- word_search.js
+++ word_search.js
@@ -2,5 +2,5 @@
   var _ = require('underscore.js');
   const dict_results = api.run("this.def_and_syn", params);
   const results = api.run("this.extract_syn", {dict_results})
-  return _.flatten(results.map(res => [res.id, ...res.syns]));
+  return _.uniq(_.flatten(results.map(res => [res.id, ...res.syns])));
 }
