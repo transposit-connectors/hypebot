2c21a996471fc9b75c795c0e4ca89b4760954fa4
Sarah Brown <sarah@transposit.com>
2019-02-05 23:58:09 +0000

    Commit from Transposit IDE

--- word_search.js
+++ word_search.js
@@ -4,7 +4,7 @@
   const results = api.run("this.extract_syn", {dict_results})
   let possible_terms =  _.uniq(_.flatten(results.map(res => [res.id, ...res.syns])));
   const thes_results = api.run("this.collegiate_thesaurus_search", params);
-  const flat_thes_results = _.flatten(thes_results.map(res => res.syns.slice(0, 2))); 
+  const flat_thes_results = _.flatten(thes_results.map(res => res.syns.slice(0, 3))); 
   possible_terms = _.uniq(possible_terms.concat(flat_thes_results));
   console.log("possible_terms", possible_terms);
   for (let i in possible_terms) {
@@ -18,5 +18,6 @@
       // nothing, the e exception message is worthless
     }
   }
+  
   return [];
 }
