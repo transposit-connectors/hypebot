2a7ba769ffff0e58778d73f688b4f4887bb58915
Sarah Brown <sarah@transposit.com>
2019-02-06 00:12:36 +0000

    Commit from Transposit IDE

--- word_search.js
+++ word_search.js
@@ -5,7 +5,7 @@
   let possible_terms =  _.uniq(_.flatten(results.map(res => [res.id, ...res.syns])));
   const thes_results = api.run("this.collegiate_thesaurus_search", params);
   const flat_thes_results = _.flatten(thes_results.map(res => res.syns.slice(0, 3))); 
-  possible_terms = _.uniq(possible_terms.concat(flat_thes_results));
+  possible_terms = _.uniq(possible_terms.concat(flat_thes_results).map(r => r.toLowerCase()));
   console.log("possible_terms", possible_terms);
   for (let i in possible_terms) {
     const sanitized = possible_terms[i].toLowerCase().replace(" ", "").replace("-", "");
@@ -18,6 +18,11 @@
       // nothing, the e exception message is worthless
     }
   }
-  
+  const res = api.run("this.search_emoji", params);
+  for (let i in res) {
+    if (res[i].emoji.moji) {
+      return res[i].emoji.moji;
+    }
+  };
   return [];
 }
