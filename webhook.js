29c1f259764feaafb08b2bfc34ec79d2725e0fd4
Sarah Brown <sarah@transposit.com>
2019-02-06 21:10:13 +0000

    Commit from Transposit IDE

--- webhook.js
+++ webhook.js
@@ -2,6 +2,10 @@
   const releaseTitle = JSON.parse(http_event.body).items[0].title;
   console.log(releaseTitle);
   
+  api.run("this.execute_crawler");
+  const crawler_results = api.run("this.get_crawler_last_execution_results");
+  console.log(crawler_results[0].releaseNotes)
+  
   return {
     status_code: 200,
     headers: { "Content-Type": "text/plain" },
