c16db5ad1d5424a9750c9eb014df16babcf5160f
Sarah Brown <sarah@transposit.com>
2019-02-07 00:29:32 +0000

    Commit from Transposit IDE

--- webhook.js
+++ webhook.js
@@ -1,11 +1,15 @@
 ({ http_event }) => {
-  const releaseTitle = JSON.parse(http_event.body).items[0].title;
-  console.log(releaseTitle);
+  const body = JSON.parse(http_event.body);
+  if (body.challenge) {
+	return {
+      status_code: 200,
+      headers: { "Content-Type": "text/plain" },
+      body: body.challenge
+    };
+  }
+
   
-  //api.run("this.execute_crawler");
-  const crawler_results = api.run("this.get_crawler_last_execution_results")[0].releaseNotes;
-  
-  const sentences = crawler_results.trim().split(/[\n\t]+/)
+/*  const sentences = crawler_results.trim().split(/[\n\t]+/)
   
   console.log(sentences);
   
@@ -14,7 +18,7 @@
   for (i=3; i<sentences.length; i++) {
     result.push(api.run("this.sentence_seeker", {sentence: sentences[i]}));
   }
-  console.log(result);
+  console.log(result);*/
   
   return {
     status_code: 200,
@@ -22,8 +26,3 @@
     body: "Thanks, buddy"
   };
 }
-
-/*
- * For sample code and reference material, visit
- * https://docs.transposit.com/building/webhooks
- */
