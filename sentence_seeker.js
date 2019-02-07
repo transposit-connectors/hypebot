4965fee8a1f910ef839a0501d3b89d85fc95f805
Sarah Brown <sarah@transposit.com>
2019-02-07 19:12:00 +0000

    Commit from Transposit IDE

--- sentence_seeker.js
+++ sentence_seeker.js
@@ -1,6 +1,6 @@
 (params) => {
   var _ = require('underscore.js');
-  return _.flatten(api.runBulk(params.sentence.replace(/\b(it|in|an|a|the|from|to|with|of|and|but|so|for)\b/g, "").split(/[ \/,:;.]+/).map(s => {
+  return _.flatten(api.runBulk(params.sentence.replace(/\b(it|in|an|a|the|from|to|with|of|and|but|so|for|are|is)\b/g, "").split(/[ \/,:;.]+/).map(s => {
     return {
       operation: "this.word_search2",
       parameters: {word: s},
