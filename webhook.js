118aef5e4d389e22a89d99af3953e6367b755ecf
Sarah Brown <sarah@transposit.com>
2019-02-08 23:16:16 +0000

    Commit from Transposit IDE

--- webhook.js
+++ webhook.js
@@ -15,7 +15,12 @@
         const { text, channel, ts } = body.event;
         console.log(text)
         const emoji = api.run("this.sentence_seeker", {sentence: text});
-        emoji.map(e => api.run("this.add_reactions", {name: e, channel , ts}));
+        if (emoji.length == 0 && .5 > Math.random()) {
+            const reactions = ["thumbsup", "star-struck", "heart_eyes", "sparkles", "heart", "clap", "ok_hand", "point_up", "sunglasses", "slightly_smiling_face"]
+          	api.run("this.add_reactions", {name: reactions[Math.floor(Math.random()*reactions.length)], channel , ts})
+        } else {
+	        emoji.map(e => api.run("this.add_reactions", {name: e, channel , ts}));
+        }
       }
     }
   }
