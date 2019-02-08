6963d46e301237205ea7615243d9783ec24a26b2
Sarah Brown <sarah@transposit.com>
2019-02-08 17:28:52 +0000

    Commit from Transposit IDE

--- webhook.js
+++ webhook.js
@@ -7,7 +7,7 @@
       body: body.challenge
     };
   } else {
-    if (body.event.user === "U8749R3T9") {
+    if (body.event.user === "U8749R3T9" || body.event.user === "U9YC7F24A") {
       const { text, channel, ts } = body.event;
       console.log(text)
       const emoji = api.run("this.sentence_seeker", {sentence: text});
