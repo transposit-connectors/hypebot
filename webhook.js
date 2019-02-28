1d39193f70910444bdb80f45ac5a009d3f9d73db
Sarah Brown <sarah@transposit.com>
2019-02-28 01:02:33 +0000

    add users_with_hype

--- webhook.js
+++ webhook.js
@@ -7,11 +7,13 @@
       body: body.challenge
     };
   } else {
-    if (body.event.user === "U8749R3T9" || body.event.user === "U9YC7F24A") {
-      if (body.event.type == "reaction_added") {
+    const { user, type } = body.event;
+    
+    if (api.run("this.users_with_hype").includes(user)) {
+      if (type == "reaction_added") {
         const { reaction, item: { channel, ts }} = body.event;
         api.run("this.add_reactions", {name: reaction, channel , ts})
-      } else {
+      } else if (type == "message") {
         const { text, channel, ts } = body.event;
         console.log(text)
         const emoji = api.run("this.sentence_seeker", {sentence: text});
