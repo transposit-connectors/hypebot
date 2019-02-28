9a247a2c9658250b81bd135aa607319557ace023
Sarah Brown <sarah@transposit.com>
2019-02-28 01:44:46 +0000

    randomHype

--- webhook.js
+++ webhook.js
@@ -1,6 +1,8 @@
 ({ http_event, hype_level }) => {
+  const randomHype = () => .1 * hype_level > Math.random()
   const body = JSON.parse(http_event.body);
   if (body.challenge) {
+    // https://api.slack.com/events/url_verification
 	return {
       status_code: 200,
       headers: { "Content-Type": "text/plain" },
@@ -8,7 +10,8 @@
     };
   } else {
     const { user, type } = body.event;
-    if (api.run("this.users_with_hype").includes(user) || .1 * hype_level > Math.random()) {
+    // don't hype everyone, all the time
+    if (api.run("this.users_with_hype").includes(user) || randomHype()) {
       if (type == "reaction_added") {
         const { reaction, item: { channel, ts }} = body.event;
         api.run("this.add_reactions", {name: reaction, channel , ts})
@@ -16,10 +19,11 @@
         const { text, channel, ts } = body.event;
         console.log(text)
         const emoji = api.run("this.sentence_seeker", {sentence: text, hype_level});
-        if (emoji.length == 0 && .1 * hype_level > Math.random()) {
-          	api.run("this.add_reactions", {name: api.run("this.positive_affirmation")[0], channel , ts})
+        if (emoji.length == 0 && randomHype()) {
+          // we didn't find anything but here's a consolation prize
+          api.run("this.add_reactions", {name: api.run("this.positive_affirmation")[0], channel , ts})
         } else {
-	        emoji.map(e => api.run("this.add_reactions", {name: e, channel , ts}));
+	      emoji.map(e => api.run("this.add_reactions", {name: e, channel , ts}));
         }
       }
     }
