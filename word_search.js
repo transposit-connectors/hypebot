1414943ee9540f84c7332d19679ec8ac4430acc1
Sarah Brown <sarah@transposit.com>
2019-03-01 00:39:46 +0000

    Commit from Transposit IDE

--- word_search.js
+++ word_search.js
@@ -1,10 +1,7 @@
 (params) => {
-  return {
-    mission: "complete"
-  };
+  const _ = require('underscore.js');
+  const synonyms = api.run("this.get_short_results", {word:params.word});
+  const possibleTerms = _.uniq([params.word, ..._.flatten(_.flatten(synonyms).map(s => s.synonyms))])
+  console.log(`possibleTerms for '${params.word}'`, possibleTerms);
+  return possibleTerms;
 }
-
-/*
- * For sample code and reference material, visit
- * https://api-composition.transposit.com/references/js-operations
- */
