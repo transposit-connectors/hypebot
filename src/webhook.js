2fa4d5725c835458713549ea881cf2e74887a387
Griffin Solot-Kehl <griffin@transposit.com>
2019-09-27 16:50:11 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -9,24 +9,26 @@
       body: body.challenge
     };
   } else {
-    const { user, type, subtype } = body.event;
-    // don't hype everyone, all the time
-    if (api.run("this.users_with_hype").includes(user) || randomHype()) {
-      if (type == "reaction_added") {
-        const { reaction, item: { channel, ts }} = body.event;
-        api.run("this.add_reactions", {name: reaction, channel , ts})
-      } else if (type == "message" && subtype != "bot_message" /* don't respond to other bots */) {
-        const { text, channel, ts } = body.event;
-        console.log(text)
-        const emoji = api.run("this.sentence_seeker", {sentence: text, hype_level});
-        if (emoji.length == 0 && randomHype()) {
-          // we didn't find anything but here's a consolation prize
-          api.run("this.add_reactions", {name: api.run("this.positive_affirmation")[0], channel , ts})
-        } else {
-	      emoji.map(e => api.run("this.add_reactions", {name: e, channel , ts}));
+    setImmediate(() => {
+      const { user, type, subtype } = body.event;
+      // don't hype everyone, all the time
+      if (api.run("this.users_with_hype").includes(user) || randomHype()) {
+        if (type == "reaction_added") {
+          const { reaction, item: { channel, ts }} = body.event;
+          api.run("this.add_reactions", {name: reaction, channel , ts})
+        } else if (type == "message" && subtype != "bot_message" /* don't respond to other bots */) {
+          const { text, channel, ts } = body.event;
+          console.log(text)
+          const emoji = api.run("this.sentence_seeker", {sentence: text, hype_level});
+          if (emoji.length == 0 && randomHype()) {
+            // we didn't find anything but here's a consolation prize
+            api.run("this.add_reactions", {name: api.run("this.positive_affirmation")[0], channel , ts})
+          } else {
+            emoji.map(e => api.run("this.add_reactions", {name: e, channel , ts}));
+          }
         }
       }
-    }
+    });
   }
   return {
     status_code: 200,
